c324d0ddd05cb3869fe211916b6ca30e343e892c
recoveryMonster <fengsongting@gmail.com>
2023-03-10 23:14:36 +0800

    chore: rollup watch feature

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import typescript from "@rollup/plugin-typescript";
 import { watch, defineConfig } from "rollup";
 
-const result = defineConfig({
+export default defineConfig({
   input: 'src/index.ts',
   output: {
     file: 'dist/index.mjs',
@@ -14,21 +14,3 @@ const result = defineConfig({
   ],
 })
 
-const watcher = watch(result);
-console.log('rollup is watching for file change...')
-
-watcher.on('event', (event) => {
-  switch (event.code) {
-    case 'START':
-      console.log('rollup is rebuilding...')
-      break
-    case 'ERROR':
-    case 'FATAL':
-      console.log('error in rebuilding.')
-      break
-    case 'END':
-      console.log('rebuild done.')
-  }
-})
-
-export default result
